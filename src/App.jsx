016af2b744971045647b7d5e22cefef57267eb70
mustafabin <mbin828@gmail.com>
2022-06-02 21:30:25 -0400

    added first draft of landing page

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,16 @@ function App() {
     <>
       <Navbar></Navbar>
       <Routes>
-        <Route path="/" element={<Landing></Landing>}></Route>
+        <Route
+          path="/"
+          element={
+            <>
+              <Landing></Landing>
+              <Landing></Landing>
+              <Landing></Landing>
+            </>
+          }
+        ></Route>
         <Route path="/aboutme" element={<Landing></Landing>}></Route>
         <Route path="/projects" element={<Landing></Landing>}></Route>
         <Route path="/resume" element={<Landing></Landing>}></Route>
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -19,18 +19,40 @@ export default function Landing() {
     <div className="landing__main__container">
       <div className="landing__page">
         <div className="landing__typewriter">
-          <Typewriter
-            options={{
-              autoStart: true,
-            }}
-            onInit={(typewriter) => {
-              typewriter
-                .typeString(screenWidth.toString())
-                .pauseFor(2500)
-                .typeString("Mustafa")
-                .start();
-            }}
-          />
+          <div className="typewriter-container">
+            <Typewriter
+              options={{
+                autoStart: true,
+              }}
+              onInit={(typewriter) => {
+                typewriter
+                  .pauseFor(2000)
+                  .typeString("Hey ! :)")
+                  .pauseFor(350)
+                  .deleteAll()
+                  .pauseFor(200)
+                  .typeString(
+                    "My name is <span style='color: var(--action-color);  font-weight: 400;'> Mustafa <span> <br></br>"
+                  )
+                  .typeString("<strong></strong>A Full Stack engeneer")
+                  .changeDeleteSpeed(30)
+                  .pauseFor(250)
+                  .deleteChars(8)
+                  .changeDelay(70)
+                  .typeString("enginere")
+                  .changeDeleteSpeed(10)
+                  .pauseFor(250)
+                  .deleteChars(8)
+                  .changeDelay("natural")
+                  .typeString(
+                    "<span style='color: var(--sub-action-color);  font-weight: 400;'>Engineer 👨🏽‍💻</span>"
+                  )
+                  .changeDeleteSpeed("natural")
+                  .pauseFor(2000)
+                  .start();
+              }}
+            />
+          </div>
         </div>
         <div className="landing__image__container">
           <img src={src} alt="Mustafa's sprite" />
