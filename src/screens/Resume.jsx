7f29a47ccf597eb2579f01a5894a7f59bb8f991b
mustafabin <mbin828@gmail.com>
2022-06-15 15:05:32 -0400

    pdf viewer

--- src/screens/Resume.jsx
+++ src/screens/Resume.jsx
@@ -1,20 +1,28 @@
 import React from "react";
-import { Document, Page } from "react-pdf";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
 import resumeSrc from "../media/resume.pdf";
 import "../styles/Resume.scss";
 export default function Resume() {
-  function onDocumentLoadSuccess({ numPages }) {
-    console.log(numPages);
+  function removeTextLayerOffset() {
+    const textLayers = document.querySelectorAll(
+      ".react-pdf__Page__textContent"
+    );
+    textLayers.forEach((layer) => {
+      const { style } = layer;
+      style.top = "0";
+      style.left = "0";
+      style.transform = "";
+    });
   }
   return (
     <div className="Resume" id="Resume">
-      <Document file={resumeSrc} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={1} />
-      </Document>
-      <a style={{ textDecoration: "none" }} download href={resumeSrc}>
-        <button className="virtual-card__button exaggerate-button">
-          Download ⬇️
-        </button>
+      <div className="Resume__pdf-container">
+        <Document className="resume-pdf" file={resumeSrc}>
+          <Page onLoadSuccess={removeTextLayerOffset} pageNumber={1} />
+        </Document>
+      </div>
+      <a className="Resume__download" download href={resumeSrc}>
+        <button>Download ⬇️</button>
       </a>
     </div>
   );
