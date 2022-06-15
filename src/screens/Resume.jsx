62c770c76d71912a0fd02a57e367a5db524026d0
mustafabin <mbin828@gmail.com>
2022-06-15 13:34:34 -0400

    added button for resume download

--- src/screens/Resume.jsx
+++ src/screens/Resume.jsx
@@ -1,9 +1,21 @@
 import React from "react";
-
+import { Document, Page } from "react-pdf";
+import resumeSrc from "../media/resume.pdf";
+import "../styles/Resume.scss";
 export default function Resume() {
+  function onDocumentLoadSuccess({ numPages }) {
+    console.log(numPages);
+  }
   return (
-    <div className="fullscreen" id="Resume">
-      Resume
+    <div className="Resume" id="Resume">
+      <Document file={resumeSrc} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={1} />
+      </Document>
+      <a style={{ textDecoration: "none" }} download href={resumeSrc}>
+        <button className="virtual-card__button exaggerate-button">
+          Download ⬇️
+        </button>
+      </a>
     </div>
   );
 }
